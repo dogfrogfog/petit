1e4f5d64f572f962108f43f4d87c2b9f2558159f
evgeniyworkbel <evgeniyworkbel@gmail.com>
2025-03-04 00:38:53 +0300

    style: update indents, disable line with require() import

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,13 +1,13 @@
 const prettierConfig = {
-	plugins: ["prettier-plugin-tailwindcss"],
-	semi: true,
-	tabWidth: 2,
-	printWidth: 120,
-	trailingComma: "all",
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: "all",
 
-	/** Settings provided by prettier-plugin-tailwindcss */ 
-	tailwindAttributes: ["titleClassName"],
-	tailwindFunctions: ["cn", "cva"],
+  /** Settings provided by prettier-plugin-tailwindcss */
+  tailwindAttributes: ["titleClassName"],
+  tailwindFunctions: ["cn", "cva"],
 };
 
 export default prettierConfig;
