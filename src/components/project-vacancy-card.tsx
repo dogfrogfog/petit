54c9f236f37c86e892d14a78c787ccde549a5bc0
evgeniyworkbel <evgeniyworkbel@gmail.com>
2025-03-04 00:29:54 +0300

    fix: remove old variant prop from badge in app code, format code

--- src/components/project-vacancy-card.tsx
+++ src/components/project-vacancy-card.tsx
@@ -31,36 +31,29 @@ export function ProjectVacancyCard({
 }) {
   return (
     <Card>
-      <CardHeader className="flex flex-wrap flex-row justify-between items-center gap-2 space-y-0">
-        <div className="gap-2 items-center flex">
-          <p className="font-semibold text-md">{vacancy.name}</p>
+      <CardHeader className="flex flex-row flex-wrap items-center justify-between gap-2 space-y-0">
+        <div className="flex items-center gap-2">
+          <p className="text-md font-semibold">{vacancy.name}</p>
           {/* <p className="text-sm text-muted-foreground">{project.name}</p> */}
         </div>
-        <Badge
-          variant={vacancy.status === "open" ? "success" : "destructive"}
-          className="capitalize"
-        >
-          {vacancy.status}
-        </Badge>
+        <Badge className="capitalize">{vacancy.status}</Badge>
       </CardHeader>
       <CardContent className="space-y-4">
         <div className="flex flex-wrap gap-2">
           {vacancy.projectDomains?.split(",").map((domain) => (
-            <Badge variant="outline" key={domain}>
+            <Badge key={domain}>
               <p className="text-sm text-muted-foreground">{domain}</p>
             </Badge>
           ))}
 
           {vacancy.projectRoles?.split(",").map((role) => (
-            <Badge variant="outline" key={role}>
+            <Badge key={role}>
               <p className="text-sm text-muted-foreground">{role}</p>
             </Badge>
           ))}
 
-          <Badge variant="outline">
-            <p className="text-sm text-muted-foreground">
-              {vacancy.expertiseLevel}
-            </p>
+          <Badge>
+            <p className="text-sm text-muted-foreground">{vacancy.expertiseLevel}</p>
           </Badge>
           {/* <div className="flex items-center gap-1">
             <DollarSign className="size-4" />
@@ -74,41 +67,28 @@ export function ProjectVacancyCard({
         {vacancy.description && (
           <div className="space-y-0">
             <p className="text-sm">Описание</p>
-            <p className="text-sm text-muted-foreground">
-              {vacancy.description}
-            </p>
+            <p className="text-sm text-muted-foreground">{vacancy.description}</p>
           </div>
         )}
 
         {vacancy.requirements && (
           <div className="space-y-0">
             <p className="text-sm">Требования</p>
-            <p className="text-sm text-muted-foreground">
-              {vacancy.requirements}
-            </p>
+            <p className="text-sm text-muted-foreground">{vacancy.requirements}</p>
           </div>
         )}
       </CardContent>
       {deleteVacancyData && (
         <CardFooter className="flex flex-wrap justify-end gap-2">
           <Button className="bg-yellow-500" asChild>
-            <Link
-              href={`/dashboard/projects/${projectId}/vacancies/${vacancy.id}/applications`}
-            >
+            <Link href={`/dashboard/projects/${projectId}/vacancies/${vacancy.id}/applications`}>
               Отклики на вакансию
             </Link>
           </Button>
           <Button variant="outline" asChild>
-            <Link
-              href={`/dashboard/projects/${projectId}/vacancies/${vacancy.id}/update`}
-            >
-              Изменить
-            </Link>
+            <Link href={`/dashboard/projects/${projectId}/vacancies/${vacancy.id}/update`}>Изменить</Link>
           </Button>
-          <Button
-            variant="destructive"
-            onClick={() => deleteVacancyData(vacancy.id)}
-          >
+          <Button variant="destructive" onClick={() => deleteVacancyData(vacancy.id)}>
             Удалить
           </Button>
         </CardFooter>
