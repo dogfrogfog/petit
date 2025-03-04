e5e0032b3685fc7d2dc8ca16992ded2c01137f5f
evgeniyworkbel <evgeniyworkbel@gmail.com>
2025-03-05 01:48:30 +0300

    fix: add  mock domain to projects

--- src/app/(allowed)/projects/page.tsx
+++ src/app/(allowed)/projects/page.tsx
@@ -1,21 +1,21 @@
 import { ProjectsList } from "@/components/projects-list";
 import { getProjectsData } from "@/lib/actions/project-data";
+import { projectDomains } from "@/lib/constants";
 import { auth } from "@clerk/nextjs/server";
 
 export default async function Projects() {
   const { userId } = await auth();
   const projects = await getProjectsData();
+  const projectsWithMockedDomain = projects.map((item) => ({ ...item, domain: projectDomains[0] }));
 
   return (
-    <div className="space-y-4 flex-1 px-4 md:px-6 mx-auto w-full py-12 max-w-2xl">
+    <div className="mx-auto w-full max-w-2xl flex-1 space-y-4 px-4 py-12 md:px-6">
       {/* Header with count and sorting */}
-      <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4 mb-6">
-        <h1 className="text-lg sm:text-xl font-semibold">
-          Found {projects.length}+ projects
-        </h1>
+      <div className="mb-6 flex flex-col items-start justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="text-lg font-semibold sm:text-xl">Found {projects.length}+ projects</h1>
       </div>
 
-      <ProjectsList projects={projects} userId={userId ?? ""} />
+      <ProjectsList projects={projectsWithMockedDomain} userId={userId ?? ""} />
     </div>
   );
 }
