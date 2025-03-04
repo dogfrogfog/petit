3caa7592134040988b2c3da92a101d20440c8a5a
evgeniyworkbel <evgeniyworkbel@gmail.com>
2025-03-05 01:08:34 +0300

    build: add prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+const prettierConfig = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: "all",
+
+  /** Settings provided by prettier-plugin-tailwindcss */
+  tailwindAttributes: ["titleClassName"],
+  tailwindFunctions: ["cn", "cva"],
+};
+
+export default prettierConfig;
